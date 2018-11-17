adc315892258af66186d4cb3479259687038aa7b
Charles Kumar <gamescharlie79@gmail.com>
2018-11-17 20:10:16 +0530

    added accordion simply then hide class implementaion

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,41 @@
-<template>
-  <div>
+<template lang="html">
+  <div id="contact-page">
     <div class="h-300 pinot-noir__gradient"></div>
-    <h1 class="c">This is an about page</h1>
+    <div class="c">
+      <h2 class="c">MV Builders <small class="mono">yours friendly Companion</small></h2>
+      <p class="para"><span>We are widely known for lucid way of choosing grounds for construction in green expansive environment. Our immense experience in apartment building with affordable price range attracts the buyers easily. We also provide clear title and legal opinion from eminent lawyers. Our floor plans according to vasthu and sense of architecture with day light optimization, ventillation optimization, etc., are quite impressive. We are well known for using quality tools in construction, quality workers and quality jobs to provide complete housing solution.</span></p>
+    </div>
+    <div class="pinot-noir__gradient">
+      <div class="c c-white hero-padding"><h2>Founder</h2>
+        <!-- <img src="https://source.unsplash.com/480x320?men" alt=""> -->
+        <div class="row">
+          <div class="col-sm2">
+            <img src="@/assets/man.jpg" alt="Founder">
+            <h5 class="mt2">K Muralidharan</h5>
+            <p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus amet consectetur, doloremque maxime molestias id, veniam nihil consequuntur nulla, tempore velit eveniet tenetur voluptates porro autem quis, iusto placeat.</p>
+          </div>
+          <div class="col-sm2 c lg-hide">
+            <h2 class="c">Decades of experience</h2>
+            <h3 class="c">Pioneer in the housing field</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-attached">
+      <img src="https://source.unsplash.com/1920x500?construction" alt="" class="mw-100">
+    </div>
+    <div class="c">
+      <h3>Feel free to contact us</h3>
+      <p>see contact page for further information.</p>
+    </div>
   </div>
 </template>
 
-<script type="text/javascript">
+<script>
 export default {
   name: "about"
 };
 </script>
+
 <style lang="scss">
 </style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -3,11 +3,56 @@
     <div class="h-300 purple-bliss__gradient"></div>
     <h1 class="c">List of Our Projects</h1>
     <p class="c"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A suscipit soluta, quis ducimus eum magni nostrum repellendus nobis, deserunt, aperiam accusantium inventore vero aliquam mollitia assumenda rem cumque, quo ea.</span><span>Voluptas commodi repellat totam, suscipit ad voluptatibus tempora vero, asperiores atque cum. Quaerat similique est laudantium, praesentium aperiam. Consequuntur quos consequatur, quam nihil minima architecto enim cum iste tempora ea?</span></p>
+    <div class="c">
+      <h2 class="c">Current Project:</h2>
+      <h3>MV Vistara</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, explicabo amet accusantium, ratione suscipit, similique repellat eos asperiores ipsam praesentium corrupti. Omnis nisi quisquam sequi, harum, iusto repellat nihil optio!</p>
+      <div class="accordion">
+        <div class="accordion-set">
+          <h3 class="accordion-title">Project 1</h3>
+          <div class="accordion-data h-show">
+            <img src="https://source.unsplash.com/1200x400?building" alt="" class="mw-100">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, ratione dignissimos eum quidem illo fuga quia. Eum quisquam, sint eveniet, excepturi vero obcaecati libero magnam, consequuntur recusandae aspernatur dolore iure!</p>
+          </div>
+        </div>
+        <div class="accordion-set">
+          <h3 class="accordion-title">Project 2</h3>
+          <div class="accordion-data">
+            <img src="https://source.unsplash.com/1201x400?building" alt="" class="mw-100">
+              <h4>Title</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non delectus iure recusandae ea, ex necessitatibus facere rerum blanditiis. Repellendus, non qui labore. Nobis possimus porro necessitatibus sed ea perspiciatis corporis.</p>
+          </div>
+        </div>
+        <div class="accordion-set">
+          <h3 class="accordion-title">Project 3</h3>
+          <div class="accordion-data">
+            <img src="https://source.unsplash.com/1202x400?building" alt="" class="mw-100">
+              <h4>Title</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae natus, itaque est modi accusantium eum voluptatibus harum laboriosam voluptates quis ducimus architecto dolorum ipsum ex! Rem ex, adipisci provident porro.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "project",
+  mounted() {
+    let accordionTriggers = document.querySelectorAll(
+      ".accordion .accordion-set h3"
+    );
+
+    for (let i = 0; i < accordionTriggers.length; i++) {
+      accordionTriggers[i].addEventListener("click", function() {
+        this.nextSibling.classList.toggle("h-show");
+        console.log(this.nextSibling);
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
