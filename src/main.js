f9ff74311049e68313678dba09105d68b55b89d2
Charles Kumar <gamescharlie79@gmail.com>
2018-11-22 11:51:17 +0530

    meta injection using vue-head effective meta description for seo

--- src/main.js
+++ src/main.js
@@ -3,11 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import stickybits from "stickybits";
 import * as help from "./js/helpers.js";
+import VueHead from "vue-head";
+
+Vue.use(VueHead);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  head: {
+    title: {
+      inner: "MV Builders - Srirangam",
+      seperator: "-"
+    },
+    meta: [
+      {
+        name: "keywords",
+        content:
+          "srirangam, flats, apartments, sale, affordable, price, railway, bus, station"
+      },
+      { name: "author", content: "MV Builders" },
+      { name: "description", content: "Buy flats in lush green environment" }
+    ]
+  },
   mounted() {
     help.attachNavEvent();
   },
