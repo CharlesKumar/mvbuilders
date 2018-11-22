89d4f9976e56c800c13fa38b10bfb4e2e4e0c101
Charles Kumar <gamescharlie79@gmail.com>
2018-11-22 12:24:57 +0530

    vue add prerender-spa-plugin just prerenders without hassle

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// const path = require("path");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");
+//
+// module.exports = {
+//   configureWebpack: config => {
+//     if (process.env.NODE_ENV === "production") {
+//       plugins: [
+//         new PrerenderSPAPlugin({
+//           staticDir: path.join(__dirname, "dist"),
+//           routes: ["/", "/about", "/project"]
+//         })
+//       ];
+//     } else {
+//       // mutate for development...
+//     }
+//   }
+// };
